7b96d95d716fa694f6221987dc4ea26d4d223699
Kinnard Cote <100028131+1naBeADev@users.noreply.github.com>
2026-02-04 08:35:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("flowslides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "flex";
+}
+
+fetch("generator/generator.html")
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById("genDiv").innerHTML = html;
+  })
+  .catch(err => console.error("Failed to load generator:", err));
